bc6dadff9005a58bb3b2ca17879edc65edb12d74
lixius-murk <bogdanasazonova45@gmail.com>
2026-03-11 20:57:29 +0300

    fixed: clean.bat

--- cean.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Cleaning up frame files...
-if exist "%TEMP%\frames" del /f /q "%TEMP%\frames"
-echo Done
---
+++ clean.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Cleaning up frame files...
+if exist "%TEMP%\frames" del /f /q "%TEMP%\frames"
+echo Done
